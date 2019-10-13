dac5972622477f3d0e860928132818ebd2f01d5b
aleks <garitonov@gmail.com>
2019-10-13 13:34:41 +0300

    4 занятие

--- src/privat/Main.java
+++ src/privat/Main.java
@@ -2,12 +2,6 @@ package privat;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(NameFactory.getCarName());
-
-
-    }
-    public static void makeService(){
-        System.out.println("текст");
+        TireFlitting.makeService();
     }
 }
